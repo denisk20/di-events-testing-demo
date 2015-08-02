941d700fa241a8051aa467ffbf9f6ea466b36fa2
Denis Kniazhev <dkniazhev@spredfast.com>
2015-08-02 09:02:35 +0300

    Wiring up dependencies

---
+++ net/src/main/java/com/denisk/android/weatherdemo/net/NetworkClient.java
@@ -0,0 +1,13 @@
+package com.denisk.android.weatherdemo.net;
+
+import com.android.volley.RequestQueue;
+
+import javax.inject.Inject;
+
+/**
+ * @author denisk
+ * @since 8/2/15.
+ */
+public class NetworkClient {
+    @Inject RequestQueue requestQueue;
+}
